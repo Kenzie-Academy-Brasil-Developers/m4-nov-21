2399e009dfed4f1dfdbaa8000a5bbcb4fbe417a8
Fabio <fabio@kenzie.com.br>
2022-07-08 09:55:05 -0300

    Config prod

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,15 +11,12 @@ const AppDataSource = new DataSource(
     } :
     {
         type: "postgres",
-        host: process.env.DB_HOST,
-        port: 5432,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB,
+        url: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === "production" ? {rejectUnauthorized: false} : false,
         logging: true,
         synchronize: false,
-        entities: ['src/entities/*.ts'],
-        migrations: ['src/migrations/*.ts']
+        entities: process.env.NODE_ENV === "production" ? ['dist/src/entities/*.js'] : ['src/entities/*.ts'],
+        migrations: process.env.NODE_ENV === "production"? ['dist/src/migrations/*.js'] : ['src/migrations/*.ts']
     }
 )
 
